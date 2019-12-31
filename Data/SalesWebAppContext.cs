27faa40e3328f1c6cdf1c99d1c98b18b8356839a
Matheus Eufrásio <matheuseufrasio2@gmail.com>
2019-12-31 11:27:09 -0300

    Criando outras Entidades

--- Data/SalesWebAppContext.cs
+++ Data/SalesWebAppContext.cs
@@ -14,5 +14,8 @@ namespace SalesWebApp.Models
         }
 
         public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
     }
 }
--- Migrations/20191231142006_OtherEntities.Designer.cs
+++ Migrations/20191231142006_OtherEntities.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebApp.Models;
 
 namespace SalesWebApp.Migrations
 {
     [DbContext(typeof(SalesWebAppContext))]
-    partial class SalesWebAppContextModelSnapshot : ModelSnapshot
+    [Migration("20191231142006_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
